3f80f8ce8aa931f77405a4eb02d33b95af12455f
franklinetush1 <mburugufrankline1@gmail..com>
2023-03-07 13:01:52 -0800

    Change variable

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     """Print the numbers from 1 to 100 separated by a space."""
     for num in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
+        if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz ", end="")
         elif num % 3 == 0 and num!= 0:
             print(f"Fizz", end=" ")
         elif num % 5 == 0 and num!= 0:
             print(f"Buzz", end=" ")
         else:
-            print("{} ".format(number), end="")
+            print("{} ".format(num), end="")
