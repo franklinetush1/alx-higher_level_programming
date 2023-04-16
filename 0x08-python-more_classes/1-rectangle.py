a19bfb56071a61863279940328fc24750365f258
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-04-16 18:48:56 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
     @property
     def width(self):
         """Set the new width"""
-        return(self.height)
+        return(self.__width)
 
     @width.setter
     def width(self, value):
@@ -30,7 +30,7 @@ class Rectangle:
     @property
     def height(self):
         """set the new height"""
-        return(self.height)
+        return(self.__height)
 
     @height.setter
     def height(self, value):
