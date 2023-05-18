7b4eb4a67924193cbbd6844a51f6911a78e4e807
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-18 18:30:26 +0300

    Update 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,4 @@
+-- Lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_showsLEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL ORDER BY tv_shows.title;
