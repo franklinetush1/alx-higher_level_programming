6cdedbd74911d8094d6c6e783e2071ef9b493315
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-18 15:30:22 +0300

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Create table id_not_null with unique id
 CREATE TABLE IF NOT EXISTS id_not_null (
-       id INT DEFAULT 1 UNIQUE,
+       id INT DEFAULT 1 UNIQUE AUTO_INCREMENT,
        name VARCHAR(256)
 );
