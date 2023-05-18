6dfadd16d3b0f8742620a9a15896a5abac56a52a
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-18 23:59:23 +0300

    Update 103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- Script that lists genres by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
