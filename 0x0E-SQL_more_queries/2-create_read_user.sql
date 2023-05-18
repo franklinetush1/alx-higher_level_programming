bb144c99c93cc7c4fdde4a37633c29e137f8528a
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-18 13:37:25 +0300

    Update 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create the database if it doesn't exist
+-- Grant SELECT privilege on the hbtn_0d_2 database to the user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
