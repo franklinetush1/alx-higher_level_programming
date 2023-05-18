b4fe3bb7b96e475699aa910bd5fe631017610ecf
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-19 00:01:17 +0300

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -5,5 +5,5 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_genres.name
+GROUP BY name
 ORDER BY rating DESC;
