f6ccc8a628113cb52f66e4614cafbd7b79ed4312
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-19 00:02:11 +0300

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,6 +3,8 @@ SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating -- Query to join tabl
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY name
