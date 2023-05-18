ad2a8b9015773027ede87136af2b45271e3e4102
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-19 00:06:11 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Create user user_0d_1 if it doesn't already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+WITH GRANT OPTION;
 FLUSH PRIVILEGES;
