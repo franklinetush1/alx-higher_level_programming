3f12445be8fcee47b323bd445410bc861268e2fc
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-12 18:16:24 +0300

    Update 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArray = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+];
+let index = 0;
+for(index;index<myArray.length;index++){
+    console.log(myArray[index]);
+}
