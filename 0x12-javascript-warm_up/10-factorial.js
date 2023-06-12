82fa171bfdbebe9dff31543bfe15fe0c50abcac6
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-12 21:21:11 +0300

    Update 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (num) {
+    if ((num === 1) || (Number.isNaN(num)) ) {
+      return 1;
+    }
+    return factorial(num - 1) * num;
+  }
+  
+  console.log(factorial(parseInt(process.argv[2])));
