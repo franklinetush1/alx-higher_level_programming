a6a3ff35c889f3bd467eb9626465b5ede3d67185
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-12 22:50:57 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const arr = process.argv.slice(2).map(Number);
-function SecondLargest(arr){
-    if (arr.length === 0) {
-      return 0;
-    } else if (arr.length === 1) {
-      return 0;
-    } else {
-      const sorted = arr.sort((a, b) => b - a);
-      return sorted[1];
-    }
+
+if (arr.length === 0) {
+  console.log(0);
+} else if (arr.length === 1) {
+  console.log(0);
+} else {
+  const sorted = arr.sort((a, b) => b - a);
+  console.log(sorted[1]);
 }
