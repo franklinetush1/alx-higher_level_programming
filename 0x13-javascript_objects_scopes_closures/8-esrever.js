00c83276995981452104da17ac445335f72ec13a
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-13 20:50:50 +0300

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function(list) {
-  var revList = [];
-  for (var i = list.length - 1; i >= 0; i--) {
+  const revList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
     revList.push(list[i]);
   }
   return revList;
