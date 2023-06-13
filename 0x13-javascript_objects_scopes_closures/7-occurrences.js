2a96dab8974266afb070fe8384e1e3afee82048a
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-13 20:44:52 +0300

    Update 7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  let size = list.length;
+  for (let x = 0; x < size; x++) {
+    if (searchElement === list[i]) {
+      count++;
+    }
+  }
+  return count;
+};
