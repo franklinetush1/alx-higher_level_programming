48274d6387149354025d1edcbd1e76897a55b012
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-17 15:14:21 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N"""
 import sys
 import MySQLdb
 if __name__ == '__main__':
