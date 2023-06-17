2780e0df8e3de897e5eb000dee2dd25e6a5e35fc
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-17 15:18:01 +0300

    Update 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""that takes in an argument and displays all values that match arg"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+	mydb = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+             .format(sys.argv[4]))
+	state = mycursor.fetchall()
+	for item in state:
+		print(item)
