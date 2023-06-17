1d25e634f5d4d0c26a70128ffec617ab86a5ef17
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-17 21:22:31 +0300

    Update 14-model_city_fetch_by_state.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_city import City
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+  result = 
