d2349981011e7e96de0fd43adb69091d2629c146
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-18 21:06:17 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     mycursor = mydb.cursor()
     mycursor.execute("SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name = '{}'".format(sys.argv[4]))
+                     FROM cities JOIN states \
+                     ON cities.state_id = states.id \
+                     WHERE states.name = '{}'".format(sys.argv[4]))
     state = mycursor.fetchall()
     for item in state:
-        print(item)
+        print(", ".join([item[1])
