75d0fc90710058052c49a5cfe3b6b54cd3738b69
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-18 18:05:52 +0300

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+	Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     result = session.query(State).filter(State.name == sys.argv[4]).first()
