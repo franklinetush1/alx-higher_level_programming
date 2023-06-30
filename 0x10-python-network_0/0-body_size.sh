2d2fb49e15ef88bbed9a2eb676f1b4c606fad69c
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-30 13:55:56 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
 # Check if the URL is provided as an argument
-if [ -z "$1" ]; then
-  exit 1
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2 
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
