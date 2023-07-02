f64f5779d0554c20559286f26af0d401937e2df4
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-02 18:56:25 +0300

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request and displays
-the value of the X-Request-Id variable found in the header
-(handling HTTP errors)
-"""
+"""Python script that takes in a URL, sends a request"""
 from urllib import request, error
 import sys
 
