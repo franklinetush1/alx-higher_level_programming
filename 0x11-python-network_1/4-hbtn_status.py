a4e7d1358c64e9dee8d684ffcb06aeee27a9e9fc
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-02 18:58:33 +0300

    Update 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetches an URL with requests package"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get('https://intranet.hbtn.io/status')
+    txt = response.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(txt), txt))
