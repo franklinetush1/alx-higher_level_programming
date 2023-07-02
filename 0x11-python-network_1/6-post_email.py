fd2d82a375e4f9ef72e302b27a12d4d0c9351c16
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-02 20:07:18 +0300

    Update 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+	arg = sys.argv[2]
+    response = requests.post(sys.argv[1], data={'email':arg})
+    print(response.text)
