9b0b239d3b156dfbb60f4141ff2dc2a2b5a6dda7
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-04 22:26:33 +0300

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,17 +4,18 @@ import sys
 import requests
 
 
-letter = sys.argv[1] if len(sys.argv[1]) > 1 else ""
-payload = {"q": letter}
-resp = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-
-try:
-    js_response = resp.json()
-    if js_response:
-        user = js_response.get("name")
-        user_id = js_response.get("id")
-        print("[{}] {}".format(user_id, user))
-    else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv[1]) > 1 else ""
+    payload = {"q": letter}
+    resp = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    
+    try:
+        js_response = resp.json()
+        if js_response:
+            user = js_response.get("name")
+            user_id = js_response.get("id")
+            print("[{}] {}".format(user_id, user))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
