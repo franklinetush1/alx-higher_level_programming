2f3f6dbc6242db9f31bdb22329fd6506def185ba
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-04 13:06:03 +0300

    Update 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that uses the GitHub API to show user id"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":    
+    user = sys.argv[1]
+    password = sys.argv[2]
+	APIurl = 'https://api.github.com/user'
+	auth = HTTPBasicAuth(user, password)
+    response = get(APIurl, auth=auth)
+    print(response.json().get('id'))
