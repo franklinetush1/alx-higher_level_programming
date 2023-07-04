5a7b32456cc7c01f02e954c5570c0558e4ab0c99
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-04 19:23:17 +0300

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Displays the value of the X-Request-Id variable"""
 import urllib.request
 import sys
-"""Displays the value of the X-Request-Id variable"""
+
 
 if __name__ == '__main__':
     url = sys.argv[1]
